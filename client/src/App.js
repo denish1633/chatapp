a98110e5cbc98da59dfb79406d9a0c6b057d6cde
denish1633 <84290782+denish1633@users.noreply.github.com>
2026-01-19 15:47:43 -0500

    Refactor routing and import statements in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
-import SignUp from "./components/Account/components.SignUp.js";
-import NoPage from "./components/Account/compomemts.NoPage.js";
-import SignIn from "./components/Account/components.SignIn.js";
-import ChatSideBar from "./components/ChatWindow.js";
-import PrivateChat from "./components/PrivateChat";
+import SignUp from "./components/Account/components.SignUp";
+import NoPage from "./components/Account/compomemts.NoPage";
+import SignIn from "./components/Account/components.SignIn";
 import Contacts from "./components/Contacts";
-import Account from "./components/Account.js";
-import ChatHomeScreen from "./components/HomeScreen.js";
-import GroupChatScreen from "./components/GroupScreen.js";
-// import UploadImage from "./components/components.UploadImage";
+import Account from "./components/Account";
+import ChatHomeScreen from "./components/HomeScreen";
+import GroupChatScreen from "./components/GroupScreen";
 
 const theme = createTheme({
   typography: {
@@ -21,29 +17,27 @@ const theme = createTheme({
   },
 });
 
-
 function App() {
   return (
-    <div>
-          <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
 
       <HashRouter>
-          <Route>
-            <Route path="Signup" element={<SignUp />} />
-            <Route path="/" element={<SignIn />} />
-            <Route path="*" element={<NoPage />} />
-            <Route path="Chat" element={<ChatHomeScreen />} />
-            <Route path="Calls" element={<Contacts />} />
-            <Route path="Teams" element={<GroupChatScreen />} />
-            <Route path="Calendar" element={<Account />} />
-            <Route path="Files" element={<Account />} />
-
-          </Route>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/Chat" element={<ChatHomeScreen />} />
+          <Route path="/Calls" element={<Contacts />} />
+          <Route path="/Teams" element={<GroupChatScreen />} />
+          <Route path="/Calendar" element={<Account />} />
+          <Route path="/Files" element={<Account />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
       </HashRouter>
-      </ThemeProvider>
 
-    </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
